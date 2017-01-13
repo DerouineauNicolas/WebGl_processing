3f1d38cfb8c7cf5c4467303399cb92f474138f97
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-01-13 16:09:13 +0100

    First rendering with tree js

--- evolution.js
+++ evolution.js
@@ -1,18 +1,17 @@
 var renderer, scene, camera;
-var geometry, material, mesh, ctx;
+//var geometry, material, mesh, ctx;
 
 
 function init(){
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
-
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.OrthographicCamera( 0, window.innerWidth , 0, window.innerHeight , 1, 1000 );
+    //camera.position.z = 0;
+    camera.position.x = 0;
+    camera.position.y = -1000;
+    camera.position.z = 0;
+    camera.lookAt(scene.position);
 
     var CANVAS=document.getElementById("your_canvas");
     renderer = new THREE.WebGLRenderer({ canvas: CANVAS } );
@@ -20,95 +19,103 @@ function init(){
 }
 
 
+
 var main=function() {
 
   var arr = [];
   function funcInJsFile(index_arg,pos_x, pos_y, radius_init, x_inc_arg, y_inc_arg) {
     // Do Stuff
-    var obj = {index: index_arg, x: pos_x, y: pos_y, radius: radius_init,  x_inc: x_inc_arg, y_inc: y_inc_arg };
+    var geometry = new THREE.BoxGeometry( radius_init, radius_init, radius_init );
+    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    var mesh = new THREE.Mesh( geometry, material );
+    //mesh.position = new THREE.Vector3(pos_x, pos_y, 0)
+
+    var obj = {m_index: index_arg,
+               m_x: pos_x, 
+               m_y: pos_y,
+               m_radius: radius_init,
+               m_x_inc: x_inc_arg, 
+               m_y_inc: y_inc_arg,
+               m_geometry: geometry,
+               m_material: material,
+               m_mesh: mesh,
+               m_isinscene: 0,
+               };
     arr.push(obj);
   }
 
   function Evoluate(Points, step_size){
           
-          if(Points.x<CANVAS.width-step_size && Points.x_inc==1){          
-             Points.x=Points.x+step_size;
+          if(Points.m_x<CANVAS.width-step_size && Points.m_x_inc==1){          
+             Points.m_x=Points.m_x+step_size;
              //console.log("1");
           }
-          else if(Points.x>step_size && Points.x_inc==0){
+          else if(Points.m_x>step_size && Points.m_x_inc==0){
              //console.log("2");
-             Points.x=Points.x-step_size;
+             Points.m_x=Points.m_x-step_size;
           }
-          else if(Points.x>(CANVAS.width-step_size)&& Points.x_inc==1){
+          else if(Points.m_x>(CANVAS.width-step_size)&& Points.m_x_inc==1){
              //console.log("3");             
-             Points.x_inc=0;
-             Points.x=Points.x-step_size;
+             Points.m_x_inc=0;
+             Points.m_x=Points.m_x-step_size;
           }
           else {
              //console.log("4");             
-             Points.x_inc=1;
-             Points.x=Points.x+step_size;
+             Points.m_x_inc=1;
+             Points.m_x=Points.m_x+step_size;
           }
 
-          if(Points.y<CANVAS.height-step_size && Points.y_inc==1){          
-             Points.y=Points.y+step_size;
+          if(Points.m_y<CANVAS.height-step_size && Points.m_y_inc==1){          
+             Points.m_y=Points.m_y+step_size;
              //console.log("1");
           }
-          else if(Points.y>step_size && Points.y_inc==0){
+          else if(Points.m_y>step_size && Points.m_y_inc==0){
              //console.log("2");
-             Points.y=Points.x-step_size;
+             Points.m_y=Points.m_y-step_size;
           }
-          else if(Points.y>(CANVAS.width-step_size)&& Points.y_inc==1){
+          else if(Points.m_y>(CANVAS.width-step_size)&& Points.m_y_inc==1){
              //console.log("3");             
-             Points.y_inc=0;
-             Points.y=Points.y-step_size;
+             Points.m_y_inc=0;
+             Points.m_y=Points.m_y-step_size;
           }
           else {
              //console.log("4");             
-             Points.y_inc=1;
-             Points.y=Points.y+step_size;
+             Points.m_y_inc=1;
+             Points.m_y=Points.m_y+step_size;
           }
   }
 
-  //alert("coucou")
 
   var CANVAS=document.getElementById("your_canvas");
   CANVAS.width=window.innerWidth;
   CANVAS.height=window.innerHeight;
 
-  step_size=100;
+  step_size=3;
 
   console.log(CANVAS.width);
   console.log(CANVAS.height);
 
   init();
 
-  funcInJsFile(0, 0+step_size,0+step_size,8,1,1);
-  funcInJsFile(1, CANVAS.width-8,CANVAS.height-8,8,0,0);
+  funcInJsFile(0, 0, 0,50,1,1);
+  funcInJsFile(1, CANVAS.width,CANVAS.height,50,0,0);
 
-  //console.log(arr.size());
-
-  x=0;
-
-  //var m_context = CANVAS.getContext(‘2d’);
 
   var animate=function() {
-      //alert("coucou")
-      /*x=x+1;
-      if(x>100)
-          return;*/
-      //
       arr.forEach(function(element) {
-          //console.log(element);
+          
+          if(element.m_isinscene==0){
+              scene.add( element.m_mesh );
+              element.m_isinscene=1;
+          }
+          element.m_mesh.position.set(element.m_x, 0, element.m_y)              
+              
+          console.log(element);
+          
           Evoluate(element,step_size);                  
       });
-      //animate(0);
 
       window.requestAnimationFrame(animate);
-
-      mesh.rotation.x += 0.01;
-      mesh.rotation.y += 0.02;
-
       renderer.render(scene, camera);      
       
   }
